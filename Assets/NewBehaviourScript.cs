7b115223bf0ed85e4ebbb930c8b3e3517d3bc1a3
lyf9375 <lyf9375@126.com>
2018-02-20 16:49:37 +1100

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+	private AndroidJavaObject jo;
+	// Use this for initialization
+	void Start () {
+		AndroidJavaClass jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");  
+		jo = jc.GetStatic<AndroidJavaObject> ("currentActivity");
+	}
+
+	public void showscreen(){
+		jo.Call ("onRTSPVRLibClicked");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
